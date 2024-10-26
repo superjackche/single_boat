c483a0b9bce5448c112ac08f15ccb3170fc8db64
superjackche <jack2276477939@gmail.com>
2024-10-26 22:50:58 +0800

    明天考计导

--- Code/2024_10_8/elephant drinks water.c
+++ Code/2024_10_8/elephant drinks water.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-char bracket_function(char* num)
-{
-    if (*num % 1 ==0)
-    {
-        return *num;
-    }
-    else
-    {
-        return(*num / 1);
-    }
-}
+#include <math.h>
+
 int main()
 {
-    int h, r, P = 3.14;
-    scanf("%d %d", &h, &r);
-    int size = P * r * r * h;
-    int result = 20 / size + 1;
-    bracket_function(result);
-    printf("%d", result);
+    double h, r;
+    scanf("%lf %lf", &h, &r);
+    double result = 20000/(3.14*r*r*h);
+    int k = trunc(ceil(result));
+    printf("%d", k);
     return 0;
 }
