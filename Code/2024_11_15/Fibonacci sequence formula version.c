30a1e724c0d1212a2c4ff2225dfa6cc258b8b9db
superjackche <jack2276477939@gmail.com>
2024-11-15 01:19:31 +0800

    www

---
+++ Code/2024_11_15/Fibonacci sequence formula version.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    long long a0 = 0;
+    double a5 = 0;
+    int n = 0;
+    a5 = sqrt(5);
+    scanf("%d", &n);
+    double a1 = pow((1+a5)/2.0, n);
+    double a2 = pow((1-a5)/2.0, n);
+    a0 = (a1-a2)/a5;
+    printf("%.2lf", a0);
+    return 0;
+}
