6e46c86ff493dd2bf13153e34927d89c029dbcf4
superjackche <jack2276477939@gmail.com>
2025-02-18 12:08:10 +0800

    yes

--- Code/2025/2025_2/2025_2_17/line.py
+++ Code/2025/2025_2/2025_2_17/line.py
@@ -1,10 +1,19 @@
 # 导包，导入Line功能构建折线图对象
 from pyecharts.charts import Line
+import pyecharts.options as opts
+from pyecharts.options import ToolboxOpts, VisualMapOpts
 
 # 得到折线图对象
 line = Line()
 # 添加x、y轴数据
 line.add_xaxis(["a", "b", "c"])
-line.add_yaxis("GDP",[10, 30, 20])
+line.add_yaxis("GDP", [10, 30, 20])
+# 设置全局配置项
+line.set_global_opts(
+    title_opts=opts.TitleOpts(title="GDP Line Chart", subtitle="Example Subtitle", pos_bottom="1%", pos_left="center"),
+    legend_opts=opts.LegendOpts(is_show=True),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True, type_="color", min_=0, max_=30)
+)
 # 生成图表
 line.render()
